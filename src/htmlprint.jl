03bae47933523bdc475dd60b9320bca08b11e8e8
WooKyoung Noh <wookay.noh@gmail.com>
2021-03-07 00:09:55 +0900

    Markdown.Code

--- src/htmlprint.jl
+++ src/htmlprint.jl
@@ -2,6 +2,5 @@ using Markdown
 
 function htmlprint(f::Function, args...; context=nothing, sizehint=0)
     s = sprint(f, args...; context, sizehint)
-    backquotes = "```"    
-    Markdown.MD(string(backquotes, s, backquotes))
+    Markdown.MD(Markdown.Code(s))
 end
